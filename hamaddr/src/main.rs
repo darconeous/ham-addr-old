a1d71d30eba8231307af0db5901e9a82a1db58f4
Dan Cross <kz2x@kz2x.ampr.org>
2021-05-05 13:01:53 +0000

    Error on bad characters instead
    Fail translation on unrepresentable characters in
    the encoding instead of of silentingly ignoring
    them.
    
    Signed-off-by: Dan Cross <kz2x@kz2x.ampr.org>

--- hamaddr/src/main.rs
+++ hamaddr/src/main.rs
@@ -1,28 +1,56 @@
-fn hamindex(c: char) -> u64 {
-    const CHARS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-\\^";
-    CHARS.find(c).unwrap_or_default() as u64 + 1
+const CHARS: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/-\\^";
+
+fn char_to_index(c: char) -> Result<u64, String> {
+    CHARS.find(c).ok_or(format!("bad char '{}'", c)).map(|k| k as u64 + 1)
 }
 
-fn triple_to_quad(chunk: &[char]) -> String {
+fn chunk_to_hex(chunk: &[char]) -> String {
     assert!(chunk.len() <= 3);
     let (quad, _) = chunk
         .into_iter()
-        .map(|c| hamindex(*c))
+        .map(|c| char_to_index(*c).unwrap())
         .fold((0, 40 * 40), |(sum, m), c| (sum + m * c, m / 40));
     format!("{:04X}", quad)
+}
+
+fn callsign_to_ham_addr(callsign: &str) -> String {
+    callsign
+        .to_uppercase()
+        .chars()
+        .collect::<Vec<_>>()
+        .chunks(3)
+        .map(|c| chunk_to_hex(c))
+        .collect::<Vec<_>>()
+        .join(":")
+}
+
+fn index_to_char(k: usize) -> Result<Option<char>, String> {
+    if k == 0 {
+        return Ok(None);
+    }
+    CHARS.chars().nth(k - 1).ok_or(format!("bad index {}", k)).map(|c| Some(c))
+}
+
+fn hex_to_chunk(hex: &str) -> Result<String, String> {
+    assert!(hex.len() <= 4);
+    let hex = format!("{:0<4}", hex);
+    let hex = u16::from_str_radix(hex.as_str(), 16).map_err(|_| "Bad hex")?;
+    let cs = [
+        index_to_char((usize::from(hex) / 1600) % 40)?,
+        index_to_char((usize::from(hex) / 40) % 40)?,
+        index_to_char((usize::from(hex) / 1) % 40)?,
+    ];
+    Ok(cs.iter().flatten().collect())
+}
+
+#[allow(dead_code)]
+fn ham_addr_to_callsign(ham_addr: &str) -> String {
+    ham_addr.split(':').map(|h| hex_to_chunk(h).unwrap()).collect::<Vec<_>>().join("")
 }
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
     for arg in &args[1..] {
-        let call = arg.to_uppercase();
-        let hamaddr = call
-            .chars()
-            .collect::<Vec<_>>()
-            .chunks(3)
-            .map(|c| triple_to_quad(c))
-            .collect::<Vec<_>>()
-            .join(":");
-        println!("call: {} -> hamaddr: {}", call, hamaddr);
+        println!("call: {} -> hamaddr: {}", arg, callsign_to_ham_addr(arg));
     }
 }
